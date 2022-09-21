96054cb03f9cff0142b7b7d55558796fdac15a5d
WestynD <70765092+WestynD@users.noreply.github.com>
2022-09-21 15:38:20 -0600

    2 files modified

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.static('public'))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 })
 
 server.listen(3000, () => {
